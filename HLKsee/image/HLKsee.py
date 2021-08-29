d4066bc9efe7d637bc0a71fa0259adbb41bfdc3f
D.E <cameronqoo@gmail.com>
2021-08-29 18:36:18 +0800

    fix bug

--- HLKsee/image/HLKsee.py
+++ HLKsee/image/HLKsee.py
@@ -2,7 +2,7 @@ import os
 import requests
 import time
 import sqlite3
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 '''
 時間阻斷器
@@ -41,20 +41,25 @@ def time_start(hour):
 '''
 預設資料庫路徑/app/data/.db
 '''
-def create_db():
-    db = sqlite3.connect('data/HLKdata.db')
-    cur = db.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS subscriber (id integer PRIMARY KEY AUTOINCREMENT, date date, name text, subscriberCount integer, videoCount integer, viewCount integer)''')
-    db.commit()
-    db.close()
+class db_method:
 
-def save_data(data):
-    db = sqlite3.connect('data/HLKdata.db')
-    cur = db.cursor()
-    cur.executemany("INSERT INTO subscriber(date, name, subscriberCount, videoCount, viewCount) VALUES (?, ?, ?, ?, ?)", data)
-    db.commit()
-    db.close()
+    def __init__(self):
+        self.db = sqlite3.connect('data/HLKdata.db')
+        self.cur = self.db.cursor()
 
+    def create_db(self):
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS subscriber (id integer PRIMARY KEY AUTOINCREMENT, date date, name text, subscriberCount integer, videoCount integer, viewCount integer)''')
+        self.db.commit()
+
+    def save_datas(self, data):
+        self.cur.executemany("INSERT INTO subscriber(date, name, subscriberCount, videoCount, viewCount) VALUES (?, ?, ?, ?, ?)", data)
+        self.db.commit()
+
+    def search_by_date(self, key):
+        return self.cur.execute("SELECT name, subscriberCount FROM subscriber WHERE date = (?)", (key,))
+    
+    def close_db(self):
+        self.db.close()
 
 if __name__=='__main__':
     print('執行時間：\n', datetime.now())
@@ -63,9 +68,12 @@ if __name__=='__main__':
     LYRA = os.getenv('LYRA')
     KUYOU = os.getenv('KUYOU')
     WEBHOOK = os.getenv('WEBHOOK')
-    create_db()
+    
+    db = db_method()
+    db.create_db()
+    db.close_db()
+    requests.post(WEBHOOK, json={"content": "HLKsee run。"}, timeout=5)
 
-    yesterday = {}
     while True:
         time_start(9)
         collect = []
@@ -82,12 +90,17 @@ if __name__=='__main__':
             data = r.json()['items'][0]['statistics']
             collect.append((ttime, name, data['subscriberCount'], data['videoCount'], data['viewCount']))
         
-        save_data(collect)
-        if yesterday != {}:
-            for i,name in enumerate(['haruka', 'lyra', 'kuyou']):
-                const += '{}: {:,} (+ {:,})\n'.format(name, int(collect[i][2]), int(collect[i][2])-int(yesterday[name]))
-                yesterday[name] = collect[i][2]
+        db = db_method()
+        db.save_datas(collect)
+
+        yesterday = db.search_by_date((date.today()-timedelta(days=1))).fetchall()
+        today = db.search_by_date(date.today()).fetchall()
+        db.close_db()
+
+        if yesterday:
+            for x, y in zip(yesterday, today):
+                const += '{}: {:,} {:+d}\n'.format(y[0], y[1], y[1]-x[1])
         else:
-            for i,name in enumerate(['haruka', 'lyra', 'kuyou']):
-                const += '{}: {:,}\n'.format(name, int(collect[i][2]))
-        response = requests.post(WEBHOOK, json={"content": const}, timeout=5)
+            for y in today:
+                const += '{}: {:,}\n'.format(y[0], y[1])
+        requests.post(WEBHOOK, json={"content": const}, timeout=5)
