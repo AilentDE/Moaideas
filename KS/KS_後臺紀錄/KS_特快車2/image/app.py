10f48d00db07841467e11b20f02f00d53ecd36c1
D.E <cameronqoo@gmail.com>
2022-07-06 12:33:32 +0800

    add KS_mini2 project

---
+++ KS/KS_後臺紀錄/KS_特快車2/image/app.py
@@ -0,0 +1,89 @@
+import os
+import time
+import math
+import pymongo
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def Bot_Message(channel, text):
+    payload={
+        "token":os.getenv('API_KEY'),
+        "channel":channel,
+        "text":'{}'.format(text)} 
+    r = requests.post("https://slack.com/api/chat.postMessage", params=payload) 
+
+if __name__=='__main__':
+    print('執行時間：\n', datetime.now())
+    MONGODB = os.getenv('MONGODB')
+    EMAIL = os.getenv('EMAIL')
+    PASS = os.getenv('PASSWORD')
+
+    try:
+        while True:
+            start = time.time()
+            rs = requests.Session()
+            url = 'https://www.kickstarter.com/login?then=%2Fprojects%2Fmoaideas%2Fmini-express-map-pack-expansion-1-and-2%2Fdashboard%3Fref%3Dcreator_nav'
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"'
+            }
+            r = rs.get(url, headers=headers, timeout=10)
+
+            myclient = pymongo.MongoClient(MONGODB)
+            db = myclient.ks_mini2
+            ttime = datetime.now()
+
+            '''
+            statu data
+            '''
+            submit = {}
+            # login (reffer dashboard)
+            url = 'https://www.kickstarter.com/user_sessions'
+            rs.headers.update({
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+                'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"'
+                })
+            data = {
+                'utf8': '✓',
+                'authenticity_token': BeautifulSoup(r.text, 'html5lib').find('input', attrs={'name': 'authenticity_token'})['value'],
+                'source': 'login',
+                'then': '/projects/moaideas/jiangnan-life-of-gentry-re/dashboard?ref=creator_nav',
+                'user_session[email]': EMAIL,
+                'user_session[password]': PASS,
+                'commit': '登入',
+                'user_session[remember_me]': '0',
+                'user_session[remember_me]': '1'
+            }
+            r = rs.post(url, data=data, timeout=10)
+            for x,y in zip(BeautifulSoup(r.text, 'html5lib').select_one('.stats-numbers').select('.gray-colored'), BeautifulSoup(r.text, 'html5lib').select_one('.stats-numbers').select('h1')):
+                submit[x.text.strip()] = y.text.strip()
+            for x,y in zip(BeautifulSoup(r.text, 'html5lib').select_one('.flex.mr2').select('.type-14'), BeautifulSoup(r.text, 'html5lib').select_one('.flex.mr2').select('.type-38')):
+                submit[x.text.strip()] = y.text.strip()
+            submit['date'] = ttime
+            collection = db.statu
+            collection.insert_one(submit)
+            print('Save STATU data at {}'.format(datetime.now()))
+
+            '''
+            reffer data
+            '''
+            submit = []
+            r = rs.get('https://www.kickstarter.com/project_referrers/refs/stats?project_id=346310841', timeout=10)
+            for i in range(1, math.ceil(r.json()['total']/20)+1):
+                r = rs.get('https://www.kickstarter.com/project_referrers/refs/stats?page={}&project_id=346310841'.format(i), timeout=10)
+                for x in r.json()['data']:
+                    x['date'] = ttime
+                    submit.append(x)
+            collection = db.reffer
+            collection.insert_many(submit)
+            print('Save REFFER data at {}'.format(datetime.now()))
+
+            myclient.close()
+            rs.close()
+            
+            time.sleep(3600-(time.time()-start))
+    except Exception as e:
+        print(e)
+        Bot_Message('spider_status', '{}'.format(e))
